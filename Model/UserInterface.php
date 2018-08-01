d5a6061b2dabdfde2ccce9c5c2440016cbcd71f6
Vladimir Strackovski <vladimir.strackovski@dlabs.si>
2018-08-01 02:11:17 +0200

    Migrated SF AdvancedUserInterface to FOS UserInterface
    Signed-off-by: Vladimir Strackovski <vladimir.strackovski@dlabs.si>

--- Model/UserInterface.php
+++ Model/UserInterface.php
@@ -11,13 +11,15 @@
 
 namespace FOS\UserBundle\Model;
 
-use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
 
 /**
  * @author Thibault Duplessis <thibault.duplessis@gmail.com>
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ * @author Julian Finkler <julian@developer-heaven.de>
  */
-interface UserInterface extends AdvancedUserInterface, \Serializable
+interface UserInterface extends BaseUserInterface, \Serializable, EquatableInterface
 {
     const ROLE_DEFAULT = 'ROLE_USER';
 
@@ -227,4 +229,52 @@ interface UserInterface extends AdvancedUserInterface, \Serializable
      * @return static
      */
     public function removeRole($role);
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired();
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked();
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired();
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled();
 }
